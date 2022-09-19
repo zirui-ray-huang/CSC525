9a3a33b6ac5d70fb5752df18c1d0ee9df0545a06
ZiruiRayHuang <ziruihuang@email.arizona.edu>
2022-09-19 01:09:20 -0700

    feat: documentation

--- Program#1/Prog1B.py
+++ Program#1/Prog1B.py
@@ -3,8 +3,16 @@ import os
 
 
 def tuple_in_record(record):
+    """
+    This function is for converting record structure into a tuple of field values
+    :param record: the record structure in characters
+    :return: the tuple of field values (name, type, date, quantity)
+    """
+
+    # extract bitmap
     bitmap = record[8:12]
 
+    # For each field, first check the corresponding value in bitmap
     # type
     if bitmap[0] == "0":
         chip_type = record[12:15]
@@ -37,12 +45,17 @@ def tuple_in_record(record):
 
 
 def records_in_block(block):
+    """
+    This function is to print filed values for records in a block
+    :param block: the block structure in characters
+    :return: null
+    """
     records = []
     num_records = int(block[:3])
     for i in range(num_records):
         offset = int(block[(6 + 6 * i): (6 + 6 * i + 3)])
         length = int(block[(9 + 6 * i): (9 + 6 * i + 3)])
-        record = tuple_in_record(block[offset: (offset + length)])
+        record = tuple_in_record(block[offset: (offset + length)])  # extract the field values for each record
         records += [record]
 
     print("{:<40} {:<5} {:<15} {:<5}".format('Product', 'Type', "Release Date", "Transistors (million)"))
@@ -56,12 +69,7 @@ def main(filepath):
     blocks = file.readlines()
 
     """
-    Part 2:
-    1. The total quantities of blocks and records contained within the DB file.
-    2. For each block, display its quantity of free space bytes.
-    3. The field values of the records held by the first block, displayed in insertion order (that is, the first record
-    displayed is the first record inserted)
-    4. Same as (3), but for the records within the last block.
+    Part 2
     """
     num_blocks = 0
     num_records = 0
@@ -91,6 +99,7 @@ def main(filepath):
     Part 3
     """
     while True:
+        # ask for user's inputs for M and N
         m = input("Enter M value: ")
         if not m.isdigit() or (m.isdigit() and int(m) == -1):
             break
@@ -102,15 +111,19 @@ def main(filepath):
 
         valid_records = []
 
+        # iterate through blocks
         for block in blocks:
             num_records = int(block[:3])
+            # iterate through records
             for i in range(num_records):
                 offset = int(block[(6 + 6 * i): (6 + 6 * i + 3)])
                 length = int(block[(9 + 6 * i): (9 + 6 * i + 3)])
                 record = tuple_in_record(block[offset: (offset + length)])
+                # save record if it saves the conditions on quantity
                 if record[3].isdigit() and m <= int(record[3]) <= n:
                     valid_records += [record]
 
+        # print the records satisfying the filtering criteria
         print("Records with # Transistors between %d and %d" % (m, n))
         print("{:<40} {:<5} {:<15} {:<5}".format('Product', 'Type', "Release Date", "Transistors (million)"))
         for record in valid_records:
